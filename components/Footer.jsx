95f64784b87ddcaeeed915f10b5aeec5e7755cae
Maria Matviian <matviyanm@gmail.com>
2023-01-16 10:03:14 -0500

    added_info_to_Navbar_and_Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { AiOutlineInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <p>2023 LogoCandles All rights reserved</p>
+      <p className="icons">
+        <AiOutlineInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,18 @@ import Link from "next/link";
 import { AiOutlineShopping } from "react-icons/ai";
 
 const Navabar = () => {
-  return <div>Navabar</div>;
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">LogoCandles</Link>
+      </p>
+
+      <button type="button" className="cart-icon" onClick="">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
+  );
 };
 
 export default Navabar;
