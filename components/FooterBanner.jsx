bea4bdb7d0b702c8198fe429b45567561ed041d8
Maria Matviian <matviyanm@gmail.com>
2023-01-15 19:02:39 -0500

    added_products_info_to_the_Product_component

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const FooterBanner = () => {
+const FooterBanner = ({ footerBanner }) => {
   return <div>FooterBanner</div>;
 };
 
--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ const Home = ({ products, bannerData }) => {
         ))}
       </div>
 
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   );
 };
