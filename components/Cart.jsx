346f60286d3806a750a3eebe65c8ad60ed88c267
Maria Matviian <matviyanm@gmail.com>
2023-01-18 17:05:25 -0500

    added_functionality_if_Cart_is_empty

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useRef } from "react";
+import Link from "next/link";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineLeft,
+  AiOutlineShopping,
+} from "react-icons/ai";
+import { TiDeleteOutline } from "react-icons/ti";
+import { toast } from "react-hot-toast";
+import { useStateContext } from "../context/StateContext";
+import { urlFor } from "../lib/client";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const cartRef = useRef();
+  const { totalPrice, totalQuantities, cartItem, setShowCart } =
+    useStateContext();
+
+  return (
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
+        {cartItem.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+            <h3>Your shopping bag is empty</h3>
+            <Link href="/">
+              <button
+                type="button"
+                className="btn"
+                onClick={() => setShowCart(false)}
+              >
+                Continue Shopping
+              </button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
