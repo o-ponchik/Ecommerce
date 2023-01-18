e62a879b6cf9877b448b567c542935d81f091218
Maria Matviian <matviyanm@gmail.com>
2023-01-18 16:19:20 -0500

    created_Context_logic

--- context/StateContext.js
+++ context/StateContext.js
@@ -5,10 +5,41 @@ const Context = createContext();
 
 export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false);
-  const [cartItem, setCartItem] = useState();
+  const [cartItem, setCartItem] = useState([]);
   const [totalPrice, setTotalPrice] = useState();
-  const [totalQuantities, setTotalQuantities] = useState();
+  const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
+
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItem.filter(
+      (item) => item._id === product._id
+    );
+
+    // change quantity of this particular product, change Total Price,
+    setTotalPrice(
+      (prevTotalPrice) => prevTotalPrice + product.price * quantity
+    );
+    setTotalQuantities((prevTotalQuantity) => prevTotalQuantity + quantity);
+
+    // if particular product is already exist in the card: update quantity of all products in the cart
+    if (checkProductInCart) {
+      const updatedCartItems = cartItem.map((cartProduct) => {
+        if (cartProduct._id === product._id) {
+          return { ...cartProduct, quantity: cartProduct.quantity + quantity };
+        }
+      });
+
+      setCartItem(updatedCartItems);
+    } else {
+      // if we add a new product to the card: change the amount of this product
+      product.quantity = quantity;
+
+      // update the cart with this product inside
+      setCartItem((prev) => [...prev, { ...product }]);
+    }
+
+    toast.success(`${qty} ${product.name} added to the cart.`);
+  };
 
   const increaseQty = () => {
     setQty((prevQty) => prevQty + 1);
@@ -26,12 +57,14 @@ export const StateContext = ({ children }) => {
     <Context.Provider
       value={{
         showCart,
+        setShowCart,
         cartItem,
         totalPrice,
         totalQuantities,
         qty,
         increaseQty,
         decreaseQty,
+        onAdd,
       }}
     >
       {children}
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -13,7 +13,7 @@ import { useStateContext } from "../../context/StateContext";
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  const { decreaseQty, increaseQty, qty } = useStateContext();
+  const { decreaseQty, increaseQty, qty, onAdd } = useStateContext();
 
   return (
     <div>
@@ -68,7 +68,11 @@ const ProductDetails = ({ product, products }) => {
           </div>
 
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick="">
+            <button
+              type="button"
+              className="add-to-cart"
+              onClick={() => onAdd(product, qty)}
+            >
               Add to Cart
             </button>
             <button type="button" className="buy-now" onClick="">
