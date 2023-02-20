7f17057920ec5bc7b305864084f5a4ef751ee3e5
Maria Matviian <matviyanm@gmail.com>
2023-02-20 16:19:11 -0500

    deleted horizontal line above form

--- pages/checkout/checkout.js
+++ pages/checkout/checkout.js
@@ -201,15 +201,6 @@ function Checkout() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppBar
-        position="absolute"
-        color="default"
-        elevation={0}
-        sx={{
-          position: "relative",
-          borderBottom: (t) => `1px solid ${t.palette.divider}`,
-        }}
-      ></AppBar>
       <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
         <Paper
           variant="outlined"
