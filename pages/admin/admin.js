93ae266e32b0cabf930f23b1c97edc11eafed4ee
Maria Matviian <matviyanm@gmail.com>
2023-02-27 16:59:48 -0500

    imported dashboard for admin page from MUI

---
+++ pages/admin/admin.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "../../components/admin/Dashboard";
+
+const Admin = () => {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+};
+
+export default Admin;
