815a04ef4e3195d3b1b71f5a7263827df2970088
Maria Matviian <matviyanm@gmail.com>
2023-03-09 18:12:29 -0500

    make list of orders on Orders page collapsed

--- components/adminPage/Orders.js
+++ components/adminPage/Orders.js
@@ -6,6 +6,11 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import Collapse from "@mui/material/Collapse";
+import Box from "@mui/material/Box";
 
 const dateTransform = (date) => {
   const transformedDate = new Date(date);
@@ -16,6 +21,7 @@ const dateTransform = (date) => {
 export default function Orders({ order, num }) {
   const { name, phone, email, details } = order.customer;
   const { street, city, country, state, zipCode } = order.customer.address;
+  const [open, setOpen] = React.useState(false);
 
   let colorStatusOrder;
   let orderStatusText;
@@ -36,7 +42,7 @@ export default function Orders({ order, num }) {
 
   const boldStyleHead = {
     fontWeight: "700",
-    color: "#004d40",
+    color: "#212121",
     fontSize: "18px",
   };
   const orderStatusStyle = {
@@ -48,97 +54,111 @@ export default function Orders({ order, num }) {
     fontWeight: "700",
     color: "black",
     fontSize: "16px",
+    backgroundColor: "#f5f5f5",
   };
 
   return (
-    <TableContainer component={Paper} style={{ marginBottom: "2rem" }}>
+    <TableContainer component={Paper} style={{ marginBottom: "1rem" }}>
       <Table sx={{ minWidth: 700 }} aria-label="spanning table" size="small">
         <TableHead style={{ backgroundColor: colorStatusOrder }}>
-          <TableRow>
+          <TableRow sx={{ "& > *": { borderBottom: "none" } }}>
+            <TableCell>
+              <IconButton
+                aria-label="expand row"
+                size="small"
+                onClick={() => setOpen(!open)}
+              >
+                {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+              </IconButton>
+            </TableCell>
             <TableCell align="left" style={boldStyleHead}>
               Order № {num}
             </TableCell>
-            <TableCell align="left" colSpan={2}>
+            <TableCell align="center">
               {dateTransform(order.createdAt)}
             </TableCell>
-            <TableCell align="center" style={orderStatusStyle}>
+            <TableCell align="right" style={orderStatusStyle} width="30%">
               {order.status}
             </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           <TableRow>
-            <TableCell style={styleHeaders} align="left">
-              Name
-            </TableCell>
-            <TableCell style={styleHeaders} align="left">
-              Email
-            </TableCell>
-            <TableCell style={styleHeaders} align="left">
-              Phone
-            </TableCell>
-            <TableCell style={styleHeaders} align="center">
-              Address
-            </TableCell>
-          </TableRow>
+            <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                <Box sx={{ margin: 1 }}>
+                  <TableRow>
+                    <TableCell style={styleHeaders} align="left" width="25%">
+                      Name
+                    </TableCell>
+                    <TableCell style={styleHeaders} align="left" width="25%">
+                      Phone
+                    </TableCell>
+                    <TableCell style={styleHeaders} align="left" width="25%">
+                      Email
+                    </TableCell>
+                    <TableCell style={styleHeaders} width="25%">
+                      Address
+                    </TableCell>
+                  </TableRow>
 
-          <TableRow>
-            <TableCell>{name}</TableCell>
-            <TableCell align="left">{phone}</TableCell>
-            <TableCell align="left">{email}</TableCell>
-            <TableCell align="center">
-              {street}, {city},{state ? state : ""},{country}, {zipCode}
-            </TableCell>
-          </TableRow>
+                  <TableRow>
+                    <TableCell>{name}</TableCell>
+                    <TableCell align="left">{phone}</TableCell>
+                    <TableCell align="left">{email}</TableCell>
+                    <TableCell>
+                      {street}, {city},{state ? state : ""},{country}, {zipCode}
+                    </TableCell>
+                  </TableRow>
 
-          <TableRow>
-            <TableCell style={styleHeaders} align="left" colSpan={2}>
-              Products
-            </TableCell>
-            <TableCell style={styleHeaders} align="center">
-              Qty
-            </TableCell>
-            <TableCell style={styleHeaders} align="center">
-              Price x 1
-            </TableCell>
-          </TableRow>
+                  <TableRow>
+                    <TableCell style={styleHeaders} align="left" colSpan={2}>
+                      Products
+                    </TableCell>
+                    <TableCell style={styleHeaders} align="left">
+                      Qty
+                    </TableCell>
+                    <TableCell style={styleHeaders}>Price x 1</TableCell>
+                  </TableRow>
 
-          {order.orderItems.map((item) => (
-            <TableRow key={item._id}>
-              <TableCell colSpan={2}>{item.name}</TableCell>
-              <TableCell align="center">{item.quantity}</TableCell>
-              <TableCell align="center">{item.price} $</TableCell>
-            </TableRow>
-          ))}
+                  {order.orderItems.map((item) => (
+                    <TableRow key={item._id}>
+                      <TableCell colSpan={2}>{item.name}</TableCell>
+                      <TableCell align="left">{item.quantity}</TableCell>
+                      <TableCell>{item.price} $</TableCell>
+                    </TableRow>
+                  ))}
 
-          <TableRow>
-            <TableCell style={styleHeaders}>Detaiils:</TableCell>
-            <TableCell colSpan={3}>{details ? details : "-"}</TableCell>
-          </TableRow>
+                  <TableRow>
+                    <TableCell style={styleHeaders}>Details:</TableCell>
+                    <TableCell colSpan={3}>{details ? details : "-"}</TableCell>
+                  </TableRow>
 
-          <TableRow>
-            <TableCell style={styleHeaders} colSpan={3}>
-              Total
-            </TableCell>
-            <TableCell style={styleHeaders} align="center">
-              {order.totalPrice} $
-            </TableCell>
-          </TableRow>
+                  <TableRow>
+                    <TableCell style={styleHeaders} colSpan={3}>
+                      Total Price:
+                    </TableCell>
+                    <TableCell style={styleHeaders}>
+                      {order.totalPrice} $
+                    </TableCell>
+                  </TableRow>
 
-          <TableRow>
-            <TableCell align="center" style={styleHeaders}>
-              Is Paid:
-            </TableCell>
-            <TableCell align="center">
-              {order.isPaid ? `Yes - ${dateTransform(order.paidAt)}` : "No"}
-            </TableCell>
-            <TableCell align="center" style={styleHeaders}>
-              Is Delivered:
-            </TableCell>
-            <TableCell align="center">
-              {order.isDelivered
-                ? `Yes - ${dateTransform(order.deliveredAt)}`
-                : "No"}
+                  <TableRow>
+                    <TableCell style={styleHeaders}>Is Paid:</TableCell>
+                    <TableCell>
+                      {order.isPaid
+                        ? `Yes - ${dateTransform(order.paidAt)}`
+                        : "No"}
+                    </TableCell>
+                    <TableCell style={styleHeaders}>Is Delivered:</TableCell>
+                    <TableCell>
+                      {order.isDelivered
+                        ? `Yes - ${dateTransform(order.deliveredAt)}`
+                        : "No"}
+                    </TableCell>
+                  </TableRow>
+                </Box>
+              </Collapse>
             </TableCell>
           </TableRow>
         </TableBody>
