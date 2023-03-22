ed16497925920e6556059d7f24d242fc117afcd2
Maria Matviian <matviyanm@gmail.com>
2023-03-22 15:12:04 -0400

    rename old Dashboard component into DashboardPanel and create a separate Dashboard component

--- components/adminPage/Dashboard.js
+++ components/adminPage/Dashboard.js
@@ -1,97 +1,12 @@
-import * as React from "react";
-import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import MuiDrawer from "@mui/material/Drawer";
-import Box from "@mui/material/Box";
-import MuiAppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import Badge from "@mui/material/Badge";
-import Container from "@mui/material/Container";
+import React from "react";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
-import MenuIcon from "@mui/icons-material/Menu";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import { ListItems } from "./ListItems.js";
 import Chart from "./Chart";
 import Deposits from "./Deposits";
 import DashboardOrders from "./DashboardOrders";
 
-import { useAdminContext } from "../../context/AdminContext";
-
-import { teal } from "@mui/material/colors";
-import Orders from "./Orders.js";
-
-const drawerWidth = 240;
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  "& .MuiDrawer-paper": {
-    position: "relative",
-    whiteSpace: "nowrap",
-    width: drawerWidth,
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    boxSizing: "border-box",
-    ...(!open && {
-      overflowX: "hidden",
-      transition: theme.transitions.create("width", {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      width: theme.spacing(7),
-      [theme.breakpoints.up("sm")]: {
-        width: theme.spacing(9),
-      },
-    }),
-  },
-}));
-
-const mdTheme = createTheme({
-  palette: {
-    primary: {
-      main: teal[800],
-    },
-  },
-});
-
-function DashboardContent({ orders }) {
-  const { showDashboard, setShowDashboard } = useAdminContext();
-
-  const qtyOfPendingOrders = orders
-    .map((order) => order.status === "Pending")
-    .filter(Boolean).length;
-
-  const [open, setOpen] = React.useState(true);
-  const toggleDrawer = () => {
-    setOpen(!open);
-  };
-
-  const dashboard = (
+const Dashboard = ({ orders }) => {
+  return (
     <>
       <Grid container spacing={3}>
         {/* Chart */}
@@ -135,85 +50,6 @@ function DashboardContent({ orders }) {
       </Grid>
     </>
   );
-  return (
-    <ThemeProvider theme={mdTheme}>
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        <AppBar position="absolute">
-          <Toolbar
-            sx={{
-              pr: "23px", // keep right padding when drawer closed
-            }}
-          >
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: "36px",
-                ...(open && { display: "none" }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              Dashboard
-            </Typography>
-            <IconButton color="inherit">
-              <Badge badgeContent={qtyOfPendingOrders} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <Drawer variant="permanent" open={open} sx={{ height: "40vh" }}>
-          <Toolbar
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-end",
-              px: [1],
-            }}
-          >
-            <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </Toolbar>
-          <Divider />
-          <ListItems />
-        </Drawer>
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === "light"
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: "100vh",
-            overflow: "auto",
-          }}
-        >
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            {showDashboard && dashboard}
-            {!showDashboard &&
-              orders.map((order, index) => (
-                <Orders order={order} key={order._id} num={index + 1} />
-              ))}
-          </Container>
-        </Box>
-      </Box>
-    </ThemeProvider>
-  );
-}
+};
 
-export default function Dashboard({ orders }) {
-  return <DashboardContent orders={orders} />;
-}
+export default Dashboard;
---
+++ components/adminPage/DashboardPanel.js
@@ -0,0 +1,211 @@
+import * as React from "react";
+
+import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiDrawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Badge from "@mui/material/Badge";
+import Container from "@mui/material/Container";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { ListItems } from "./ListItems.js";
+
+import { useAdminContext } from "../../context/AdminContext";
+
+import { teal } from "@mui/material/colors";
+
+import Link from "next/link";
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    }),
+  },
+}));
+
+const mdTheme = createTheme({
+  palette: {
+    primary: {
+      main: teal[800],
+    },
+  },
+});
+
+const DashboardContent = ({ children, orders }) => {
+  const qtyOfPendingOrders = orders
+    .map((order) => order.status === "Pending")
+    .filter(Boolean).length;
+
+  const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
+  // const dashboard = (
+  //   <>
+  //     <Grid container spacing={3}>
+  //       {/* Chart */}
+  //       <Grid item xs={12} md={8} lg={9}>
+  //         <Paper
+  //           sx={{
+  //             p: 2,
+  //             display: "flex",
+  //             flexDirection: "column",
+  //             height: 240,
+  //           }}
+  //         >
+  //           <Chart />
+  //         </Paper>
+  //       </Grid>
+  //       {/* Recent Deposits */}
+  //       <Grid item xs={12} md={4} lg={3}>
+  //         <Paper
+  //           sx={{
+  //             p: 2,
+  //             display: "flex",
+  //             flexDirection: "column",
+  //             height: 240,
+  //           }}
+  //         >
+  //           <Deposits />
+  //         </Paper>
+  //       </Grid>
+  //       {/* Recent Orders */}
+  //       <Grid item xs={12}>
+  //         <Paper
+  //           sx={{
+  //             p: 2,
+  //             display: "flex",
+  //             flexDirection: "column",
+  //           }}
+  //         >
+  //           <DashboardOrders orders={orders} />
+  //         </Paper>
+  //       </Grid>
+  //     </Grid>
+  //   </>
+  // );
+
+  return (
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar position="absolute">
+          <Toolbar
+            sx={{
+              pr: "23px", // keep right padding when drawer closed
+            }}
+          >
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: "36px",
+                ...(open && { display: "none" }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              Dashboard
+            </Typography>
+            <IconButton color="inherit">
+              <Badge badgeContent={qtyOfPendingOrders} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open} sx={{ height: "40vh" }}>
+          <Toolbar
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-end",
+              px: [1],
+            }}
+          >
+            <IconButton onClick={toggleDrawer}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <ListItems orders={orders} />
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto",
+          }}
+        >
+          <Container maxWidth="lg" sx={{ mt: 11, mb: 4 }}>
+            {children}
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+export default function DashboardPanel({ orders, children }) {
+  return <DashboardContent orders={orders}>{children}</DashboardContent>;
+}
