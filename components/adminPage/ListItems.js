603d58870078cf97ff6a0a0e4d05c39c4b97f6a9
Maria Matviian <matviyanm@gmail.com>
2023-03-22 15:13:25 -0400

    add links with related path for all list positions

--- components/adminPage/ListItems.js
+++ components/adminPage/ListItems.js
@@ -2,57 +2,52 @@ import * as React from "react";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import ListSubheader from "@mui/material/ListSubheader";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PeopleIcon from "@mui/icons-material/People";
 import BarChartIcon from "@mui/icons-material/BarChart";
 import LayersIcon from "@mui/icons-material/Layers";
-import AssignmentIcon from "@mui/icons-material/Assignment";
 import Link from "next/link";
-import List from "@mui/material/List";
-
-import { useAdminContext } from "../../context/AdminContext";
 
 export function ListItems() {
-  const { showDashboard, setShowDashboard } = useAdminContext();
-
   return (
     <React.Fragment>
-      <ListItemButton
-        onClick={() => {
-          console.log("clicked on dashboard");
-          setShowDashboard(true);
-        }}
-      >
-        <ListItemIcon>
-          <DashboardIcon />
-        </ListItemIcon>
-        <ListItemText primary="Dashboard" />
-      </ListItemButton>
-      <ListItemButton
-        onClick={() => {
-          console.log("clicked on orders");
-          setShowDashboard(false);
-        }}
-      >
-        <ListItemIcon>
-          <ShoppingCartIcon />
-        </ListItemIcon>
-        <ListItemText primary="Orders" />
-      </ListItemButton>
-      <ListItemButton>
-        <ListItemIcon>
-          <PeopleIcon />
-        </ListItemIcon>
-        <ListItemText primary="Customers" />
-      </ListItemButton>
-      <ListItemButton>
-        <ListItemIcon>
-          <BarChartIcon />
-        </ListItemIcon>
-        <ListItemText primary="Reports" />
-      </ListItemButton>
+      <Link href={"/admin/dashboard"}>
+        <ListItemButton>
+          <ListItemIcon>
+            <DashboardIcon />
+          </ListItemIcon>
+          <ListItemText primary="Dashboard" />
+        </ListItemButton>
+      </Link>
+      <Link href={"/admin/list/orders"}>
+        <ListItemButton>
+          <ListItemIcon>
+            <ShoppingCartIcon />
+          </ListItemIcon>
+
+          <ListItemText primary="Orders" />
+        </ListItemButton>
+      </Link>
+
+      <Link href={"/admin/list/customers"}>
+        <ListItemButton>
+          <ListItemIcon>
+            <PeopleIcon />
+          </ListItemIcon>
+          <ListItemText primary="Customers" />
+        </ListItemButton>
+      </Link>
+
+      <Link href={"/admin/list/reports"}>
+        <ListItemButton>
+          <ListItemIcon>
+            <BarChartIcon />
+          </ListItemIcon>
+          <ListItemText primary="Reports" />
+        </ListItemButton>
+      </Link>
+
       <ListItemButton>
         <ListItemIcon>
           <LayersIcon />
