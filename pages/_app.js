33baaa0266220e2f3e7195fcaceaa3b927392fae
Maria Matviian <matviyanm@gmail.com>
2023-04-08 18:29:54 -0400

    upgrade Next, React and MUI to the latest version

--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,30 @@ import { Toaster } from "react-hot-toast";
 import { Layout } from "../components";
 import "../styles/globals.css";
 import { StateContext } from "../context/StateContext";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme();
 
 export default function App({ Component, pageProps }) {
   if (Component.getLayout) {
-    return Component.getLayout(<Component {...pageProps} />);
+    return Component.getLayout(
+      <>
+        <ThemeProvider theme={theme}>
+          <Toaster />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </>
+    );
   }
 
   return (
     <StateContext>
-      <Layout>
-        <Toaster />
-        <Component {...pageProps} />
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Toaster />
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </StateContext>
   );
 }
