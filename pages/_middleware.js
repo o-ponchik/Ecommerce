938faab2cee38a85c86431e2470f1695821670b7
Maria Matviian <matviyanm@gmail.com>
2023-04-08 18:31:11 -0400

    protect routes

---
+++ pages/_middleware.js
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import { jwtVerify } from "jose";
+
+export async function middleware(request) {
+  const jwt = request.cookies?.OursiteJWT;
+  console.log(jwt);
+
+  if (request.nextUrl.pathname.includes("/admin")) {
+    if (!jwt) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    try {
+      const { payload } = await jwtVerify(
+        jwt,
+        new TextEncoder().encode("secret")
+      );
+      console.log(payload);
+    } catch (err) {
+      console.error({ err });
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin/dashboard", "/admin/list/orders"],
+};
