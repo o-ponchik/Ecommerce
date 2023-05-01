c4cace0c48f6e2935ffd39ca597ddd3e72fb790f
Maria Matviian <matviyanm@gmail.com>
2023-05-01 14:32:50 -0400

    add links to the instagram, email and phone

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,28 @@
 import React from "react";
-import { AiOutlineInstagram, AiOutlineTwitter } from "react-icons/ai";
+import {
+  AiOutlineInstagram,
+  AiOutlinePhone,
+  AiOutlineMail,
+} from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <p>2023 LogoCandles All rights reserved</p>
       <p className="icons">
-        <AiOutlineInstagram />
-        <AiOutlineTwitter />
+        <a
+          href={"https://instagram.com/rosy___fox?igshid=YmMyMTA2M2Y="}
+          target="_blank"
+          className="instagram"
+        >
+          <AiOutlineInstagram />
+        </a>
+        <a href="mailto: abc@example.com">
+          <AiOutlineMail />
+        </a>
+        <a href="tel:+380637009106">
+          <AiOutlinePhone />
+        </a>
       </p>
     </div>
   );
